1090e3310a6a86e968ab37e49bb1eba012fa4599
AndriusDulevicius <andriusdulevicius@email.com>
2021-08-30 10:28:53 +0300

    meetupDetail done

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,10 +1,16 @@
-import useRouter from 'next/router';
+import { useRouter } from 'next/router';
+import MeetupDetail from './../../components/meetups/MeetupDetail';
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   const meetupObj = useRouter();
   return (
     <>
-      <h2>Details of meet with id: {meetupObj.query.meetupId}</h2>
+      <MeetupDetail
+        title='The first meetup'
+        image='https://picsum.photos/id/1060/1000/800'
+        address='kazkoks 12'
+        description='firs meet in italy'
+      />
     </>
   );
 };
