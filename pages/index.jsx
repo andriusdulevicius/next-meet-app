84fcc3aba7dc529b01dcbe45792d349c7061b814
AndriusDulevicius <andriusdulevicius@email.com>
2021-08-30 09:43:07 +0300

    meetup form added, layout used

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,35 @@
+import MeetupList from './../components/meetups/MeetupList';
+import Layout from './../components/layout/Layout';
+
+const DUMMY_MEETUPS = [
+  {
+    id: '1',
+    title: 'The first meetup',
+    image: 'https://picsum.photos/id/1018/1000/800',
+    address: 'Baker street 10, 1231231, London, GB',
+    description: 'First meet in London',
+  },
+  {
+    id: '2',
+    title: 'The first meetup in Italy',
+    image: 'https://picsum.photos/id/1060/1000/800',
+    address: 'Baker street 10, 1231231, Rome, Italy',
+    description: 'First meet in Italy',
+  },
+  {
+    id: '3',
+    title: 'The first meetup in Greece',
+    image: 'https://picsum.photos/id/1000/1000/800',
+    address: 'Baker street 10, 1231231, Greece',
+    description: 'First meet in Greece',
+  },
+];
+
 const HomePage = () => {
   return (
-    <>
-      <h1>Home meetup page</h1>
-    </>
+    <Layout>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </Layout>
   );
 };
 
--- pages/new-meetup/index.jsx
+++ pages/new-meetup/index.jsx
@@ -1,8 +1,16 @@
+import { loadGetInitialProps } from 'next/dist/next-server/lib/utils';
+import NewMeetupForm from './../../components/meetups/NewMeetupForm';
+import Layout from './../../components/layout/Layout';
 const NewMeetup = () => {
+  const addMeetupHandler = (enteredData) => {
+    console.log(enteredData);
+  };
+
   return (
-    <>
+    <Layout>
       <h2>Add new meetup here</h2>
-    </>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Layout>
   );
 };
 
