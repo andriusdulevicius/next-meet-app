01ddb71d75adf95fb5554874f4e176cf9c48f09c
AndriusDulevicius <andriusdulevicius@email.com>
2021-08-31 09:00:43 +0300

    get static path using ids from mongo db

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,3 +1,4 @@
+import { MongoClient } from 'mongodb';
 import { useRouter } from 'next/router';
 import MeetupDetail from './../../components/meetups/MeetupDetail';
 
@@ -16,40 +17,46 @@ const MeetupDetails = (props) => {
 };
 
 // reikalinga funkcija , generuoti statinius puslapius
-export function getStaticPaths() {
-  return {
-    fallback: true, //einant i psl kurio nera aprasyta paths, puslapis sugeneruojams uzklausos metu(at run time), jei false - tai nesant pathui nuves i 404
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(process.env.MONGO_CONN);
+  const db = client.db();
+  // sukurti arba nusitiaikyti i esama
+  const meetupCollecion = db.collection('meetups');
+  const allMeets = await meetupCollecion.find({}).toArray();
+  client.close();
+  console.log('All meeets transformed ============');
+  const pathsArrOfCurrentMeets = allMeets.map((oneMeet) => {
+    return {
+      params: {
+        meetupId: oneMeet._id.toString(),
+      },
+    };
+  });
+  console.log(pathsArrOfCurrentMeets);
 
-    paths: [
-      {
-        params: {
-          meetupId: '1',
-        },
-      },
-      {
-        params: {
-          meetupId: '2',
-        },
-      },
-      {
-        params: {
-          meetupId: '3',
-        },
-      },
-    ],
+  return {
+    fallback: false, //einant i psl kurio nera aprasyta paths, puslapis sugeneruojams uzklausos metu(at run time), jei false - tai nesant pathui nuves i 404
+    paths: pathsArrOfCurrentMeets,
   };
 }
 
-export function getStaticProps(context) {
-  console.log(context.params.meetupId);
+export async function getStaticProps(context) {
+  const client = await MongoClient.connect(process.env.MONGO_CONN);
+  const db = client.db();
+  // sukurti arba nusitiaikyti i esama
+  const meetupCollecion = db.collection('meetups');
+  const allMeets = await meetupCollecion.find({}).toArray();
+  client.close();
+  allMeets;
+
   return {
     props: {
       meetupData: {
-        id: context.params.meetupId,
-        title: 'The first meetup',
-        image: 'https://picsum.photos/id/1018/1000/800',
-        address: 'Baker street 10, 1231231, London, GB',
-        description: 'First meet in London',
+        // id: context.params.meetupId,
+        // title: 'The first meetup',
+        // image: 'https://picsum.photos/id/1018/1000/800',
+        // address: 'Baker street 10, 1231231, London, GB',
+        // description: 'First meet in London',
       },
       revalidate: 5, //kas kiek sekundziu duomenys bus atnaujinami
     },
